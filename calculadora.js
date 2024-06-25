3b095640b0df9fc5043e0d87008ae2de64391d2d
Job <chirinosjob@gmail.com>
2024-06-25 18:06:51 -0400

    third commit

--- calculadora.js
+++ calculadora.js
@@ -3,7 +3,8 @@ let numeros = document.querySelectorAll('.numeros')
 let signos = document.querySelectorAll('.signo')
 let borrarUno = document.getElementById('delete')
 let historial = document.getElementById('historial')
-let valor = campoOperaciones.value.slice(-1)
+let punto = document.querySelector('.punto')
+let arrSigno = ['+', '-', 'x', '÷', '^', '.']
 
 numeros.forEach((elemento) =>{
     elemento.addEventListener('click', () => {
@@ -15,198 +16,52 @@ numeros.forEach((elemento) =>{
     })
 })
 
+let expresionRegular = /\.?/
+punto.addEventListener('click', () => {
+    if(!arrSigno.includes(campoOperaciones.value.slice(-1)) && campoOperaciones.value.length > 0){
+        if(expresionRegular.test(campoOperaciones.value)){
+            let textNum = punto.textContent
+            campoOperaciones.value += textNum              
+         }
+    }
+})
+
 signos.forEach((elemento) =>{
     elemento.addEventListener('click', () => {
         if(campoOperaciones.value.length > 0 &&
-            campoOperaciones.value.slice(-1) != '+' &&
-            campoOperaciones.value.slice(-1) != '-' &&
-            campoOperaciones.value.slice(-1) != 'x' &&
-            campoOperaciones.value.slice(-1) != '÷' &&
-            campoOperaciones.value.slice(-1) != '^' 
+            !arrSigno.includes(campoOperaciones.value.slice(-1))
         ){
             let textNum = elemento.textContent
-        campoOperaciones.value += textNum
+            campoOperaciones.value += textNum
         }
     })
 })
 
+borrarUno.addEventListener('click', () =>{
+    if(campoOperaciones.value.length > 0){
+        let nuevoValor = campoOperaciones.value.slice(0, -1)
+        campoOperaciones.value = nuevoValor
+    }
+})
 
+let btnTransition = document.getElementById('switch')
+btnTransition.addEventListener('click', () => {
+    document.body.classList.toggle('dark')
+    btnTransition.classList.toggle('active')
 
-let light = document.getElementById('light')
-let dark = document.getElementById('dark')
-let con = document.getElementById('contenedor-principal')
-let conForm = document.getElementById('formulario')
-let conBotones = document.getElementById('botones')
-let botones = document.querySelectorAll('#botones button')
-let borradores = document.querySelectorAll('.yellow')
-let transitions = document.getElementById('boton-transition')
-let ldTransition = document.querySelectorAll('.ld')
-let classInactiva = document.querySelectorAll('.inactiva')
-
-light.addEventListener('click', () => {
-    document.body.classList.remove('dark')
-    document.body.classList.add('light')
-
-    conForm.classList.remove('inactiva')
-    conForm.classList.add('activa')
-
-    con.classList.remove('inactiva')
-    con.classList.add('activa')
-
-    campoOperaciones.classList.remove('inactiva')
-    campoOperaciones.classList.add('activa')
-    campoOperaciones.style.color = '#000000'
-
-    conBotones.classList.remove('inactiva')
-    conBotones.classList.add('activa')
-
-    transitions.classList.remove('inactiva')
-    transitions.classList.add('activa')
-
-    ldTransition.forEach((elemento) => {
-        elemento.classList.remove('inactiva')
-        elemento.classList.add('activa')
-        elemento.style.color = '#000000'
-    })
-
-    botones.forEach((elemento) => {
-        elemento.classList.remove('inactiva')
-        elemento.classList.add('activa')
-    })
-
-    numeros.forEach((elemento)=>{
-        elemento.style.color = '#000000'
-    })
-    
-    borradores.forEach((elemento)=>{
-        elemento.style.color = '#000000'
-    })
-
-    if(document.body.classList.contains('light')){
+    if(document.body.classList.contains('dark')){
+        localStorage.setItem('dark-mode', 'true')
+    }else{
         localStorage.setItem('dark-mode', 'false')
     }
 })
 
-dark.addEventListener('click', () => {
-    document.body.classList.remove('light')
-    document.body.classList.add('dark')
-
-    conForm.classList.remove('activa')
-    conForm.classList.add('inactiva')
-
-    con.classList.remove('activa')
-    con.classList.add('inactiva')
-    
-    campoOperaciones.classList.remove('activa')
-    campoOperaciones.classList.add('inactiva')
-    campoOperaciones.style.color = '#ffffff'
-
-    conBotones.classList.remove('activa')
-    conBotones.classList.add('inactiva')
-
-    transitions.classList.remove('activa')
-    transitions.classList.add('inactiva')
-
-    ldTransition.forEach((elemento) => {
-        elemento.classList.remove('activa')
-        elemento.classList.add('inactiva')
-        elemento.style.color = '#ffffff'
-    })
-
-    botones.forEach((elemento) => {
-        elemento.classList.remove('activa')
-        elemento.classList.add('inactiva')
-    })
-
-    numeros.forEach((elemento)=>{
-        elemento.style.color = '#ffffff'
-    })
-
-    borradores.forEach((elemento)=>{
-        elemento.style.color = '#ffffff'
-    })
-
-    if(document.body.classList.contains('dark')){
-        localStorage.setItem('dark-mode', 'true')
-    }
-})
-
 if(localStorage.getItem('dark-mode') === 'true'){
-    document.body.classList.remove('light')
     document.body.classList.add('dark')
-
-    conForm.classList.remove('activa')
-    conForm.classList.add('inactiva')
-
-    con.classList.remove('activa')
-    con.classList.add('inactiva')
-    
-    campoOperaciones.classList.remove('activa')
-    campoOperaciones.classList.add('inactiva')
-    campoOperaciones.style.color = '#ffffff'
-
-    conBotones.classList.remove('activa')
-    conBotones.classList.add('inactiva')
-
-    transitions.classList.remove('activa')
-    transitions.classList.add('inactiva')
-
-    ldTransition.forEach((elemento) => {
-        elemento.classList.remove('activa')
-        elemento.classList.add('inactiva')
-        elemento.style.color = '#ffffff'
-    })
-
-    botones.forEach((elemento) => {
-        elemento.classList.remove('activa')
-        elemento.classList.add('inactiva')
-    })
-
-    numeros.forEach((elemento)=>{
-        elemento.style.color = '#ffffff'
-    })
-
-    borradores.forEach((elemento)=>{
-        elemento.style.color = '#ffffff'
-    })
+    btnTransition.classList.add('active')
 }else{
     document.body.classList.remove('dark')
-    document.body.classList.add('light')
-
-    conForm.classList.remove('inactiva')
-    conForm.classList.add('activa')
-
-    con.classList.remove('inactiva')
-    con.classList.add('activa')
-
-    campoOperaciones.classList.remove('inactiva')
-    campoOperaciones.classList.add('activa')
-    campoOperaciones.style.color = '#000000'
-
-    conBotones.classList.remove('inactiva')
-    conBotones.classList.add('activa')
-
-    transitions.classList.remove('inactiva')
-    transitions.classList.add('activa')
-
-    ldTransition.forEach((elemento) => {
-        elemento.classList.remove('inactiva')
-        elemento.classList.add('activa')
-        elemento.style.color = '#000000'
-    })
-
-    botones.forEach((elemento) => {
-        elemento.classList.remove('inactiva')
-        elemento.classList.add('activa')
-    })
-
-    numeros.forEach((elemento)=>{
-        elemento.style.color = '#000000'
-    })
-    
-    borradores.forEach((elemento)=>{
-        elemento.style.color = '#000000'
-    })
+    btnTransition.classList.remove('active')
 }
 
 let igual = document.getElementById('igualdad')
@@ -221,76 +76,45 @@ borrarTodo.addEventListener('click', () => {
 
 let identificarEjecutar = () => {
     igual.addEventListener('click', () => {
-        historial.innerHTML = `<p>${campoOperaciones.value}</p>`
+        if(!arrSigno.includes(campoOperaciones.value.slice(-1))){
+            historial.innerHTML = `<p>${campoOperaciones.value}</p>`
 
         let cant = campoOperaciones.value 
         let arreglo = cant.split('')
         arreglo.filter((op) => {
             switch(op) {
-                case '+':
-                    preresultado = arreglo.join('')
-                    resultado = preresultado.split('+')
-                    campoOperaciones.value = parseFloat(resultado[0]) + parseFloat(resultado[1])
-                    break
-                case '-':
-                    preresultado = arreglo.join('')
-                    resultado = preresultado.split('-')
-                    campoOperaciones.value = parseFloat(resultado[0]) - parseFloat(resultado[1])
-                    break
-
-                case 'x':
-                    preresultado = arreglo.join('')
-                    resultado = preresultado.split('x')
-                    campoOperaciones.value = parseFloat(resultado[0]) * parseFloat(resultado[1])
-                    break
-                case '÷':
-                    preresultado = arreglo.join('')
-                    resultado = preresultado.split('÷')
-                    campoOperaciones.value = parseFloat(resultado[0]) / parseFloat(resultado[1])
-                    break
-                case '^':
-                    preresultado = arreglo.join('')
-                    resultado = preresultado.split('^')
-                    campoOperaciones.value = parseFloat(resultado[0]) ** parseFloat(resultado[1])
-                    break
+        case '+':
+            preresultado = arreglo.join('')
+            resultado = preresultado.split('+')
+            campoOperaciones.value = parseFloat(resultado[0]) + parseFloat(resultado[1])
+            break
+        case '-':
+            preresultado = arreglo.join('')
+            resultado = preresultado.split('-')
+            campoOperaciones.value = parseFloat(resultado[0]) - parseFloat(resultado[1])
+            break
+        case 'x':
+            preresultado = arreglo.join('')
+            resultado = preresultado.split('x')
+            campoOperaciones.value = parseFloat(resultado[0]) * parseFloat(resultado[1])
+            break
+        case '÷':
+            preresultado = arreglo.join('')
+            resultado = preresultado.split('÷')
+            campoOperaciones.value = parseFloat(resultado[0]) / parseFloat(resultado[1])
+            break
+        case '^':
+            preresultado = arreglo.join('')
+            resultado = preresultado.split('^')
+            campoOperaciones.value = parseFloat(resultado[0]) ** parseFloat(resultado[1])
+            break
             }
-            // if(op === '+'){
-            //     let preresultado = arreglo.join('')
-            //     let resultado = preresultado.split('+')
-            //     campoOperaciones.value = parseFloat(resultado[0]) + parseFloat(resultado[1])
-            // }else if(op === '-'){
-            //     let preresultado = arreglo.join('')
-            //     let resultado = preresultado.split('-')
-            //     campoOperaciones.value = parseFloat(resultado[0]) - parseFloat(resultado[1])
-            // }else if(op === 'x'){
-            //     let preresultado = arreglo.join('')
-            //     let resultado = preresultado.split('x')
-            //     campoOperaciones.value = parseFloat(resultado[0]) * parseFloat(resultado[1])
-            // }else if(op === '÷'){
-            //     let preresultado = arreglo.join('')
-            //     let resultado = preresultado.split('÷')
-            //     campoOperaciones.value = parseFloat(resultado[0]) / parseFloat(resultado[1])
-            // }else if(op === '^'){
-            //     let preresultado = arreglo.join('')
-            //     let resultado = preresultado.split('^')
-            //     campoOperaciones.value = parseFloat(resultado[0]) ** parseFloat(resultado[1])
-            // }
-            
         })
-        let valor = campoOperaciones.value
-        if(valor.length > 16){
+        if(campoOperaciones.value.length > 16){
             campoOperaciones.style.fontSize = '20px'
         }
+        }
+        
     })
 }
-
 identificarEjecutar()
-
-borrarUno.addEventListener('click', () =>{
-    let cantidad = campoOperaciones.value
-
-    if(cantidad.length > 0){
-        let nuevoValor = campoOperaciones.value.slice(0, -1)
-        campoOperaciones.value = nuevoValor
-    }
-})
